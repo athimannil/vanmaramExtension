67be6b88e43bd400a4d5468667461396cc9dbf27
Muhammed Athimannil <hello@athimannil.com>
2024-12-21 14:17:21 +0100

    Migrate to Manifest V3
    Migrate the Chrome extension to Manifest V3.
    
    * **manifest.json**
      - Update `manifest_version` to 3.
      - Replace `browser_action` with `action`.
      - Update `content_security_policy` to "script-src 'self'; object-src 'self'".
    
    * **popup.html**
      - Update `ng-app` directive to "ng-app='app'".
      - Add `defer` attribute to script tags.
    
    * **script.js**
      - Wrap code in an IIFE (Immediately Invoked Function Expression).
      - Add AngularJS module definition and controller registration.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/athimannil/vanmaramExtension?shareId=XXXX-XXXX-XXXX-XXXX).

--- script.js
+++ script.js
@@ -1,63 +1,67 @@
-function customersController($scope, $http) {
-    var target, searchword;
-    // Toggle input clear
-    $scope.toggleInput = function (argument) {
-        $scope.cleardata();
-    };
-    $scope.search = function(searchUrl) {
-        //Check for searchUrl, otherwise use keywords.
-        if ($scope.languagevalue){
-            target = 'ml';
-            searchUrl = typeof(searchUrl) != 'undefined' ? searchUrl : document.getElementById('english').value;
-            document.getElementById('english').value = searchUrl;
-        }else{
-            target = 'en';
-            searchUrl = typeof(searchUrl) != 'undefined' ? searchUrl : $scope.enkeywords;
-            $scope.enkeywords = searchUrl;
-        }
+(function() {
+    var app = angular.module('app', []);
 
-        $scope.url = 'https://www.vanmaram.com/json_result.php?'+target+'='+searchUrl;
-        $http.get($scope.url).
-        success(function(data, status) {
-            $scope.status = status;
-            $scope.searchword = searchUrl;
-            $scope.result = data; // Show result from server in <li> element
-            $scope.suggetionresult = null;
-        }).
-        error(function(data, status) {
-            $scope.data = data || "Request failed";
-            $scope.status = status;
-        });
-    };
-    $scope.suggestword = function () {
-        if ($scope.languagevalue){
-            target = 'ml';
-            searchUrl = $scope.mlkeywords;
-        }else{
-            target = 'en';
-            searchUrl = $scope.enkeywords;
-        }
-        $scope.url = 'https://www.vanmaram.com/ajax_json_suggestion.php?'+target+'='+searchUrl; // The url of our search
-        $http.get($scope.url).
-        success(function(data, status) {
-            $scope.status = status;
-            $scope.suggetionresult = data; // Show result from server in <li> element
+    app.controller('customersController', ['$scope', '$http', function($scope, $http) {
+        var target, searchword;
+        // Toggle input clear
+        $scope.toggleInput = function (argument) {
+            $scope.cleardata();
+        };
+        $scope.search = function(searchUrl) {
+            //Check for searchUrl, otherwise use keywords.
+            if ($scope.languagevalue){
+                target = 'ml';
+                searchUrl = typeof(searchUrl) != 'undefined' ? searchUrl : document.getElementById('english').value;
+                document.getElementById('english').value = searchUrl;
+            }else{
+                target = 'en';
+                searchUrl = typeof(searchUrl) != 'undefined' ? searchUrl : $scope.enkeywords;
+                $scope.enkeywords = searchUrl;
+            }
+
+            $scope.url = 'https://www.vanmaram.com/json_result.php?'+target+'='+searchUrl;
+            $http.get($scope.url).
+            success(function(data, status) {
+                $scope.status = status;
+                $scope.searchword = searchUrl;
+                $scope.result = data; // Show result from server in <li> element
+                $scope.suggetionresult = null;
+            }).
+            error(function(data, status) {
+                $scope.data = data || "Request failed";
+                $scope.status = status;
+            });
+        };
+        $scope.suggestword = function () {
+            if ($scope.languagevalue){
+                target = 'ml';
+                searchUrl = $scope.mlkeywords;
+            }else{
+                target = 'en';
+                searchUrl = $scope.enkeywords;
+            }
+            $scope.url = 'https://www.vanmaram.com/ajax_json_suggestion.php?'+target+'='+searchUrl; // The url of our search
+            $http.get($scope.url).
+            success(function(data, status) {
+                $scope.status = status;
+                $scope.suggetionresult = data; // Show result from server in <li> element
+                $scope.result = null;
+            }).
+            error(function(data, status) {
+                $scope.data = data || "Request failed";
+                $scope.status = status;
+            });
+            
+        };
+        $scope.cleardata = function (argument) {
+            $scope.enkeywords = '';
+            // $scope.enkeywords = '';
+            document.getElementById('english').value = '';
             $scope.result = null;
-        }).
-        error(function(data, status) {
-            $scope.data = data || "Request failed";
-            $scope.status = status;
-        });
-        
-    };
-    $scope.cleardata = function (argument) {
-        $scope.enkeywords = '';
-        // $scope.enkeywords = '';
-        document.getElementById('english').value = '';
-        $scope.result = null;
-        $scope.suggetionresult = null;
-    };
-    $scope.manglish = function () {
-        transliterateKey();
-    };
-}
+            $scope.suggetionresult = null;
+        };
+        $scope.manglish = function () {
+            transliterateKey();
+        };
+    }]);
+})();
